091adf0f5a47c724aae275a92a8c79fef3e5fd48
Mateo-Buytaert <6738@tiffin.kingston.sch.uk>
2025-03-26 14:32:45 +0000

    fixing dotenv bug

--- recipeazy/settings.py
+++ recipeazy/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv, find_dontenv
+from dotenv import load_dotenv, find_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(find_dotenv())
