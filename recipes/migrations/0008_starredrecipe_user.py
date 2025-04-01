e00f64d8215dfb182b7a0b587f1d48bd7ea43938
Mateo Buytaert <6738@tiffin.kingston.sch.uk>
2025-04-01 17:19:10 +0100

    Update 0008_starredrecipe_user.py

--- recipes/migrations/0008_starredrecipe_user.py
+++ recipes/migrations/0008_starredrecipe_user.py
@@ -16,6 +16,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='starredrecipe',
             name='user',
-            field=models.ForeignKey(default='mateo', on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
         ),
     ]
