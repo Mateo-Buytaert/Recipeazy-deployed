c2668831a74aafe3cc88df3a8f3010b473325710
Mateo Buytaert <6738@tiffin.kingston.sch.uk>
2025-04-04 16:13:33 +0100

    Update sitemaps.py

--- recipeazy/sitemaps.py
+++ recipeazy/sitemaps.py
@@ -1,1 +1,20 @@
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+from recipes.models import Recipe
 
+class StaticViewSitemap(Sitemap):
+    priority = 0.5
+    changefreq = 'daily'
+
+    def items(self):
+        return ['home', 'about']  # Your named URL patterns
+
+    def location(self, item):
+        return reverse(item)
+
+class RecipeSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.8
+
+    def items(self):
+        return Recipe.objects.all()
