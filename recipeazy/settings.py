cb904488a660c53e36bf2319c8595f56695b2f86
Mateo Buytaert <6738@tiffin.kingston.sch.uk>
2025-04-04 15:22:06 +0100

    Update settings.py

--- recipeazy/settings.py
+++ recipeazy/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "recipes.apps.RecipesConfig"
+    "recipes.apps.RecipesConfig",
+    "django.contrib.sitemaps"
 ]
 
 MIDDLEWARE = [
